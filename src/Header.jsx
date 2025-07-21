c37267351ffe850b6532012390a1073400d5bcca
Prasad7659 <pkrupavara@gmail.com>
2025-07-21 23:25:40 +0530

    Modified Header styles

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import Title from "./Title";
 
 export default function Header(){
     return (
-        <div className="header" style={{display: "flex", flexWrap: "wrap", border:"1px solid #45A29E", margin:"5px"}}>
+        <div className="header" style={{display: "flex", flexWrap: "wrap", /*border:"1px solid #45A29E"*/ margin:"5px"}}>
             <Title/>
             <Navbar/>
         </div>
